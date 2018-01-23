1d8e89b937a5beb6fc89b59a1f32e62958558124
Abhishek Thakur <abhithakurg1@gmail.com>
2018-01-23 16:43:09 +0530

    Default Code

---
+++ app/src/main/java/com/example/iblink/myapplication/ForCall.kt
@@ -0,0 +1,21 @@
+package com.example.iblink.myapplication
+
+import android.util.Log
+import org.linphone.core.LinphoneCall
+import org.linphone.core.LinphoneCore
+
+
+/**
+ * Created by iblink on 1/23/2018.
+ */
+
+abstract class ForCall : LinphoneCall {
+
+    override fun isAuthenticationTokenVerified(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/iblink/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/iblink/myapplication/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.iblink.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+import org.linphone.*
+import org.linphone.core.LinphoneCall
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sipaddress = findViewById<TextView>(R.id.editText2)
+        val sipaddressvalue = sipaddress.text.toString()
+        Log.i("address check","$sipaddressvalue")
+
     }
 }
